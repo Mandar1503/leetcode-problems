b55456399d6e0b993de11d96d78f98653a7e3c91
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2021-12-27 15:43:15 -0500

    Time: 8 ms (6.87%), Space: 9.3 MB (7.37%) - LeetHub

--- add-binary/add-binary.cpp
+++ add-binary/add-binary.cpp
@@ -31,3 +31,34 @@ public:
         return ans;
     }
 };
+
+
+//NOT WORKING HAVE TO LOOK INTO THE SOLUTION
+// class Solution {
+// public:
+//     string addBinary(string a, string b) {
+//         unsigned long int no1=0;
+//         unsigned long int no2=0;
+//         for(int i=0;i<a.size();i++)
+//         {
+//             no1=no1*10 + (a[i]-'0');
+//         }
+//         for(int i=0;i<b.size();i++)
+//         {
+//             no2=no2*10 + (b[i]-'0');
+//         }
+//         unsigned long int answer;
+//         unsigned long int carry;
+//         while(no2!=0)
+//         {
+//             answer = no1^no2;
+//             carry = (no1&no2)<<1;
+//         }
+//         string ans;
+//         stringstream ss;
+//         ss<<answer;
+//         ss>>ans;
+        
+//         return ans;
+//     }
+// };
