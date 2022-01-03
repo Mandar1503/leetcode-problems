ded6b78aab9a2a4a6b0b070977956f69b9a038e0
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-01-03 18:16:05 -0500

    Time: 28 ms (5.75%), Space: 18.8 MB (54.44%) - LeetHub

---
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//RECURSIVE APPROACH
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if(!root)
+//             return 0;
+        
+//         return max(maxDepth(root->left),maxDepth(root->right))+1;
+//     }
+// };
+
+
+//This ITERATIVE APPROACH is done by implementing bfs only not storing the nodes, just incrementing the depth counter after each loop i.e. each level
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+       
+        if(!root)
+            return 0;
+        queue<TreeNode*> q;
+        
+        q.push(root);
+        int depth =0;
+        
+        while(!q.empty())
+        {
+            depth++;
+            int count = q.size();
+            for(int i=0;i<count;i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if(node->left)
+                    q.push(node->left);
+            
+                if(node->right)
+                    q.push(node->right);
+            }
+        }
+        return depth;   
+    }    
+};
