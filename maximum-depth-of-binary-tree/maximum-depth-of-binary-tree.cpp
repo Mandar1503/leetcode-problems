82783ee15a513eedb18acd14c4ad954c90295e0e
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-01-03 18:16:46 -0500

    Time: 15 ms (11.47%), Space: 18.8 MB (54.44%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,46 +11,46 @@
  */
 
 //RECURSIVE APPROACH
-// class Solution {
-// public:
-//     int maxDepth(TreeNode* root) {
-//         if(!root)
-//             return 0;
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
         
-//         return max(maxDepth(root->left),maxDepth(root->right))+1;
-//     }
-// };
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
+    }
+};
 
 
 //This ITERATIVE APPROACH is done by implementing bfs only not storing the nodes, just incrementing the depth counter after each loop i.e. each level
 
-class Solution {
-public:
-    int maxDepth(TreeNode* root) {
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
        
-        if(!root)
-            return 0;
-        queue<TreeNode*> q;
+//         if(!root)
+//             return 0;
+//         queue<TreeNode*> q;
         
-        q.push(root);
-        int depth =0;
+//         q.push(root);
+//         int depth =0;
         
-        while(!q.empty())
-        {
-            depth++;
-            int count = q.size();
-            for(int i=0;i<count;i++)
-            {
-                TreeNode* node = q.front();
-                q.pop();
+//         while(!q.empty())
+//         {
+//             depth++;
+//             int count = q.size();
+//             for(int i=0;i<count;i++)
+//             {
+//                 TreeNode* node = q.front();
+//                 q.pop();
 
-                if(node->left)
-                    q.push(node->left);
+//                 if(node->left)
+//                     q.push(node->left);
             
-                if(node->right)
-                    q.push(node->right);
-            }
-        }
-        return depth;   
-    }    
-};
+//                 if(node->right)
+//                     q.push(node->right);
+//             }
+//         }
+//         return depth;   
+//     }    
+// };
