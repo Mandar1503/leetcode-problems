d6d23a6681a0226c492f3def375a20094f41c856
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-01-06 17:20:28 -0500

    Time: 54 ms (21.02%), Space: 19.7 MB (64.74%) - LeetHub

---
+++ design-linked-list/design-linked-list.cpp
@@ -0,0 +1,129 @@
+struct Node{
+    int val;
+    Node* next;
+    Node(int val)
+    {
+        this->val = val;
+        next = NULL;
+    }
+};
+
+class MyLinkedList {
+public:
+    int size;
+    Node *head;
+    
+    MyLinkedList() 
+    {
+        size = 0;
+        head = NULL;
+    }
+    
+    
+    int get(int index) 
+    {
+        
+        if(index < 0 || index>=size)
+            return -1;
+        
+        Node* curr = head;
+        while(index>0)
+        {
+            curr=curr->next;
+            index--;
+        }
+        return curr->val;
+    }
+    
+    
+    void addAtHead(int val) 
+    {
+        Node* newnode = new Node(val);
+        if(head==NULL)
+        {
+            head = newnode;
+        }
+        else
+        {
+            newnode->next = head;
+            head=newnode;
+        }
+        size++;
+    }
+    
+    void addAtTail(int val) 
+    {    
+        Node* newnode = new Node(val);
+        
+        if(head==NULL)
+        {
+            head = newnode;
+        }
+        else
+        {
+            Node* curr = head;
+            while(curr->next!=NULL)
+                curr=curr->next;
+            curr->next = newnode;
+        }
+        size++;
+    }
+    
+    void addAtIndex(int index, int val) 
+    {
+        if(index<0 || index>size)
+            return;
+        
+        if(index==0)
+            addAtHead(val);
+        
+        else if(index==size)
+            addAtTail(val);
+        
+        else
+        {
+            Node* newnode = new Node(val);
+            Node* curr = head;
+            while(index>1)
+            {
+                curr=curr->next;
+                index--;
+            }
+            newnode->next = curr->next;
+            curr->next = newnode;
+            size++;
+        }
+    }
+    
+    void deleteAtIndex(int index) {
+        
+        if(index<0 || index>=size)
+            return;
+        
+        if(index==0)
+        {
+            head=head->next;
+            size--;
+            return;
+        }
+        
+        Node* curr = head;
+        while(index>1)
+        {
+            curr=curr->next;
+            index--;
+        }
+        curr->next=curr->next->next;
+        size--;
+    }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
