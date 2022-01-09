34bcea7f22713bf908c6b367fb046b5c7e449224
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-01-08 21:28:01 -0500

    Time: 35 ms (9.62%), Space: 15 MB (43.21%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -10,64 +10,65 @@
  */
 
 //My Approach works pretty good
-// class Solution {
-// public:
-//     ListNode* removeElements(ListNode* head, int val) {
-//         ListNode* temp =head;
-        
-//         while(head!=NULL && head->val==val)
-//         {
-//             head = head->next; 
-//             temp = head;
-//         }
-//         if(head==NULL)
-//             return head;
-//         while(temp->next!=NULL)
-//         {
-//             if(temp->next->val==val)
-//             {
-//                 temp->next = temp->next->next;
-//             }
-//             else
-//                 temp=temp->next;
-//         }
-//         return head;
-//     }
-// };
-
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode* sentinel = new ListNode(0);
-        sentinel->next = head;
+        ListNode* temp =head;
         
-        ListNode* prev = sentinel;
-        ListNode* curr = head;
-        ListNode* toDelete = NULL;
-        
-        while(curr!=NULL)
+        while(head!=NULL && head->val==val)
         {
-            if(curr->val==val)
+            head = head->next; 
+            temp = head;
+        }
+        if(head==NULL)
+            return head;
+        while(temp->next!=NULL)
+        {
+            if(temp->next->val==val)
             {
-                prev->next=curr->next;
-                toDelete = curr;
+                temp->next = temp->next->next;
             }
             else
-            {
-                prev = curr;
-            }
-            curr=curr->next;
-            
-            if(toDelete!=NULL)
-            {
-                delete toDelete;
-                toDelete = NULL;
-            }
+                temp=temp->next;
         }
-        
-        ListNode* ans = sentinel->next;
-        delete sentinel;
-        return ans;
+        return head;
     }
 };
+
+//Sentinel Approach from solution
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         ListNode* sentinel = new ListNode(0);
+//         sentinel->next = head;
+        
+//         ListNode* prev = sentinel;
+//         ListNode* curr = head;
+//         ListNode* toDelete = NULL;
+        
+//         while(curr!=NULL)
+//         {
+//             if(curr->val==val)
+//             {
+//                 prev->next=curr->next;
+//                 toDelete = curr;
+//             }
+//             else
+//             {
+//                 prev = curr;
+//             }
+//             curr=curr->next;
+            
+//             if(toDelete!=NULL)
+//             {
+//                 delete toDelete;
+//                 toDelete = NULL;
+//             }
+//         }
+        
+//         ListNode* ans = sentinel->next;
+//         delete sentinel;
+//         return ans;
+//     }
+// };
 
