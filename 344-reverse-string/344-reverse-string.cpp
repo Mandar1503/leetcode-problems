2d9bdbf6a4408b199a301c785560d0f6f33876f0
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-01-09 21:04:28 -0500

    Time: 949 ms (5.26%), Space: 36.5 MB (39.89%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,33 +1,33 @@
 
 //MY Method Time=O(N), Space = O(N)(recursion stack)
-// class Solution {
-// public:
-//     void reverseString(vector<char>& s) {
-//        if(s.empty())
-//            return;
-//         char ele = s.back();
-//         s.pop_back();
-//         reverseString(s);
-//         s.insert(s.begin(),ele);
-//     }
-// };
-
-//Using two pointer => Time= O(N)(total N/2 swaps), Space = O(N)(recursion stack)
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-       helper(s,0,s.size()-1);
-    }
-    void helper(vector<char>& s,int left,int right)
-    {
-        if(left>=right)
-            return;
-        char temp = s[left];
-        s[left++] = s[right];
-        s[right--] = temp;
-        helper(s,left,right);
+       if(s.empty())
+           return;
+        char ele = s.back();
+        s.pop_back();
+        reverseString(s);
+        s.insert(s.begin(),ele);
     }
 };
+
+//Using two pointer => Time= O(N)(total N/2 swaps), Space = O(N)(recursion stack)
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//        helper(s,0,s.size()-1);
+//     }
+//     void helper(vector<char>& s,int left,int right)
+//     {
+//         if(left>=right)
+//             return;
+//         char temp = s[left];
+//         s[left++] = s[right];
+//         s[right--] = temp;
+//         helper(s,left,right);
+//     }
+// };
 
 //Using Two Pointers but not Recursion => Time = O(N), Space = O(1)(since no extra space used done inplace)
 // class Solution {
