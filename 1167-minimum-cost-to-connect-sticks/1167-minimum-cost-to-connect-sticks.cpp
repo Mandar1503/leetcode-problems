a981b463cbd9487fd502a2eae2c54efeb0484412
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-01-26 20:22:00 -0500

    Time: 110 ms (46.82%), Space: 24.2 MB (22.35%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
@@ -1,44 +1,4 @@
 //My orignal solution Time= O(nlogn)
-class Solution {
-public:
-    int connectSticks(vector<int>& sticks) {
-        priority_queue<int,vector<int>,greater<int>> minheap;
-        
-        for(int i=0;i<sticks.size();i++)
-        {
-            minheap.push(sticks[i]);
-        }
-        
-        if(minheap.size()==1)
-            return 0;
-        
-        vector<int> costs;
-        
-        while(!minheap.empty())
-        {
-            int no1 = minheap.top();
-            minheap.pop();
-            
-            int no2 = minheap.top();
-            minheap.pop();
-            
-            int sum = no1 + no2;
-            costs.push_back(sum);
-            minheap.push(sum);
-            
-            if(minheap.size()==1)
-                break;
-        }
-        int cost = 0;
-        for(int i=0;i<costs.size();i++)
-        {
-            cost+=costs[i];
-        }
-        return cost;
-    }
-};
-
-//my more optimized solution
 // class Solution {
 // public:
 //     int connectSticks(vector<int>& sticks) {
@@ -48,8 +8,13 @@ public:
 //         {
 //             minheap.push(sticks[i]);
 //         }
-//         int cost = 0;
-//         while(minheap.size()>1)
+        
+//         if(minheap.size()==1)
+//             return 0;
+        
+//         vector<int> costs;
+        
+//         while(!minheap.empty())
 //         {
 //             int no1 = minheap.top();
 //             minheap.pop();
@@ -57,10 +22,45 @@ public:
 //             int no2 = minheap.top();
 //             minheap.pop();
             
-//             cost+=(no1+no2);
+//             int sum = no1 + no2;
+//             costs.push_back(sum);
+//             minheap.push(sum);
             
-//             minheap.push(no1+no2);
+//             if(minheap.size()==1)
+//                 break;
+//         }
+//         int cost = 0;
+//         for(int i=0;i<costs.size();i++)
+//         {
+//             cost+=costs[i];
 //         }
 //         return cost;
 //     }
 // };
+
+//my more optimized solution = Time = O(nlogn) but less space complexity
+class Solution {
+public:
+    int connectSticks(vector<int>& sticks) {
+        priority_queue<int,vector<int>,greater<int>> minheap;
+        
+        for(int i=0;i<sticks.size();i++)
+        {
+            minheap.push(sticks[i]);
+        }
+        int cost = 0;
+        while(minheap.size()>1)
+        {
+            int no1 = minheap.top();
+            minheap.pop();
+            
+            int no2 = minheap.top();
+            minheap.pop();
+            
+            cost+=(no1+no2);
+            
+            minheap.push(no1+no2);
+        }
+        return cost;
+    }
+};
