3e256f2c391caf348124051ba13a8a79f0ec3aab
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-01-29 12:28:55 -0500

    Time: 5 ms (7.42%), Space: 6.1 MB (36.71%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -25,25 +25,46 @@
 
 
 //Soln using memoization
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         int* memo = new int[n+1];
+//         return climb_stairs(0,n,memo);
+//     }
+    
+//     int climb_stairs(int i, int n, int memo[])
+//     {
+//         if(i>n)
+//             return 0;
+        
+//         if(i==n)
+//             return 1;
+        
+//         if(memo[i]>0)
+//             return memo[i];
+        
+//         memo[i] = climb_stairs(i+1,n,memo) + climb_stairs(i+2,n,memo);
+//         return memo[i];
+//     }
+// };
+
+
+
+//DP
 class Solution {
 public:
     int climbStairs(int n) {
-        int* memo = new int[n+1];
-        return climb_stairs(0,n,memo);
-    }
-    
-    int climb_stairs(int i, int n, int memo[])
-    {
-        if(i>n)
-            return 0;
-        
-        if(i==n)
+        if(n==1)
             return 1;
         
-        if(memo[i]>0)
-            return memo[i];
+        int* dp = new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
         
-        memo[i] = climb_stairs(i+1,n,memo) + climb_stairs(i+2,n,memo);
-        return memo[i];
+        for(int i=3;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
