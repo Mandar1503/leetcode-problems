abdb48d1f6c776c86235f221951a8457e24e3a43
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-03-27 22:25:07 -0400

    Time: 0 ms (100.00%), Space: 6.2 MB (79.71%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,23 +1,42 @@
 //MY SOLUTION
+// class Solution {
+// public:
+//     bool isValid(string s) {
+        
+//         stack<char> brkts;
+        
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(brkts.empty() && (s[i] == ')' || s[i] == '}' || s[i] == ']'))
+//                 return false;
+//             else if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+//                 brkts.push(s[i]);
+//             else if(((s[i] == '}' && brkts.top() == '{') || (s[i] == ')' && brkts.top() == '(') || (s[i] == ']' && brkts.top() == '[')) && !brkts.empty())
+//             {
+//                 brkts.pop();   
+//             }
+//             else
+//                 return false;
+//         }
+//         return brkts.empty();        
+//     }
+// };
+
 class Solution {
 public:
     bool isValid(string s) {
-        
-        stack<char> brkts;
-        
-        for(int i=0;i<s.size();i++)
+         stack<char> brkts;
+        for(auto i:s)
         {
-            if(brkts.empty() && (s[i] == ')' || s[i] == '}' || s[i] == ']'))
-                return false;
-            else if(s[i] == '(' || s[i] == '{' || s[i] == '[')
-                brkts.push(s[i]);
-            else if(((s[i] == '}' && brkts.top() == '{') || (s[i] == ')' && brkts.top() == '(') || (s[i] == ']' && brkts.top() == '[')) && !brkts.empty())
-            {
-                brkts.pop();   
-            }
+            if(i == '(' || i == '{' || i == '[')
+                brkts.push(i);
             else
-                return false;
+            {
+                if(brkts.empty() || (i != '}' && brkts.top() == '{') || (i != ')' && brkts.top() == '(') || (i != ']' && brkts.top() == '['))
+                    return false;    
+                brkts.pop();
+            }
         }
-        return brkts.empty();        
+        return brkts.empty();
     }
 };
