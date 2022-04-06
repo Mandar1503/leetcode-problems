be64f311d99f7f3517d14cbef4957e36fea65600
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-06 02:01:25 -0400

    Time: 10 ms (68.11%), Space: 7.2 MB (78.31%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,24 +1,52 @@
-//Brute force
+//Another solution(GOOD)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> characters;
+        
         if(s.size()!=t.size())
             return false;
-        for(char ch : s)
+        
+        int counter[26] = {0};
+        
+        for(int i=0;i<s.size();i++)
         {
-            if(characters.find(ch)!=characters.end())
-                characters[ch]++;
-            else
-                characters.insert(pair<char,int>(ch,1));
+            counter[s[i]-'a']++;
+            counter[t[i]-'a']--;
         }
-        for(char ch : t)
+        
+        for(int i=0;i<26;i++)
         {
-            if(characters.find(ch)!=characters.end() && characters[ch]!=0)
-                characters[ch]--;        
-            else
+            if(counter[i]!=0)
                 return false;
         }
-        return true;
+       return true;
     }
 };
+
+//Brute force (BETTER SOLUTION)
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         map<char,int> characters;
+        
+//         if(s.size()!=t.size())
+//             return false;
+        
+//         for(char ch : s)
+//         {
+//             if(characters.find(ch)!=characters.end())
+//                 characters[ch]++;
+//             else
+//                 characters.insert(pair<char,int>(ch,1));
+//         }
+        
+//         for(char ch : t)
+//         {
+//             if(characters.find(ch)!=characters.end() && characters[ch]!=0)
+//                 characters[ch]--;        
+//             else
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
