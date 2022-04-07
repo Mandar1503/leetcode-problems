c3939d63739aea6a15fff5b00d8847069755ba68
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-07 14:31:44 -0400

    Time: 1 ms (81.72%), Space: 42.9 MB (18.04%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,22 +1,43 @@
 //horizontal scanning
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+        
+//         if(strs.length == 0) 
+//             return "";
+        
+//         String p = strs[0];     //take one string
+        
+//         for(int i=1;i<strs.length;i++) //iterate through next string
+//         {
+//             while(strs[i].indexOf(p)!=0) //if it is not a substring then 
+//             {
+//                 p = p.substring(0,p.length()-1);//reduce length of the substring
+                
+//                 if(p.isEmpty()) //if the string becomes empty then return ""
+//                     return "";
+//             }
+//         }
+//         return p;
+//     }
+// }
+
+//vertical approach
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         
-        if(strs.length == 0) 
+        if(strs==null || strs.length==0)
             return "";
         
-        String p = strs[0];     //take one string
-        
-        for(int i=1;i<strs.length;i++) //iterate through next string
+        for(int i=0;i<strs[0].length();i++)
         {
-            while(strs[i].indexOf(p)!=0) //if it is not a substring then 
+            char c = strs[0].charAt(i);
+            for(int j=1;j<strs.length;j++)
             {
-                p = p.substring(0,p.length()-1);//reduce length of the substring
-                
-                if(p.isEmpty()) //if the string becomes empty then return ""
-                    return "";
+                if(i==strs[j].length() || strs[j].charAt(i)!=c)
+                    return strs[0].substring(0,i);
             }
         }
-        return p;
+        
+        return strs[0];
     }
 }
