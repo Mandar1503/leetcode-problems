98bcd556fc03c8a95a3ef645b54b5d1863dbc3f8
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-09 14:14:54 -0400

    Time: 11 ms (98.32%), Space: 42 MB (88.88%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,23 @@
+//DP SOLUTION
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        //declare a dp array and take the size of the array as amount+1 to be max
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1); //fill the array by amount+1 max size
+        
+        dp[0]=0;            //for the base case
+            
+        for(int i=1;i<=amount;i++)  //to iterate & compute for all the values from 0 to goal amount
+        {
+            for(int j=0;j<coins.length;j++) //to check for each of the coin
+            {
+                if(coins[j]<=i)     //if coin is greater than the current amount i then do not consider it 
+                    dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);   //calculate the appropriate best/min number of coins that can be used to compute the amount using each of the coin
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];  //if the dp[amount] is still greater than amount then we know that there is no way to get the amount using those coins.
+    }
+}
+
+//DP SOLUTION MORE OPTIMIZED
