97cfb857ba0ef07dd2047c4928f9b56e2d88d70c
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-09 14:26:50 -0400

    Time: 18 ms (83.69%), Space: 45.7 MB (30.39%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,7 +1,7 @@
 //DP SOLUTION
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        
+        Arrays.sort(coins);
         //declare a dp array and take the size of the array as amount+1 to be max
         int[] dp = new int[amount+1];
         Arrays.fill(dp,amount+1); //fill the array by amount+1 max size
@@ -14,10 +14,35 @@ class Solution {
             {
                 if(coins[j]<=i)     //if coin is greater than the current amount i then do not consider it 
                     dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);   //calculate the appropriate best/min number of coins that can be used to compute the amount using each of the coin
+                else
+                    break;
             }
         }
         return dp[amount]>amount?-1:dp[amount];  //if the dp[amount] is still greater than amount then we know that there is no way to get the amount using those coins.
     }
 }
 
-//DP SOLUTION MORE OPTIMIZED
+
+//DP SOLUTION MORE OPTIMIZED (sort the array of coins and do not go ahead for the coins that are greater than the amount)
+
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         Arrays.sort(coins);
+        
+//         int dp[] = new int[amount+1];
+        
+//         Arrays.fill(dp,amount+1);
+        
+//         for(int i=1;i<=amount;i++)
+//         {
+//             for(int j=0;j<coins.length;j++)
+//             {
+//                 if(coins[j]<=i)
+//                     dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);
+//                 else
+//                     break;
+//             }
+//         }
+//         return dp[amount]>amount?-1:dp[amount];
+//     }
+// }
