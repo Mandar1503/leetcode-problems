bdb0bfc1a8ae26b42d0ac6a076a40afb7aea2d17
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-09 18:32:40 -0400

    Time: 14 ms (72.23%), Space: 50.7 MB (40.87%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+        }
+        
+        // System.out.print(freq);
+        
+        PriorityQueue<Pair<Integer,Integer>> minheap = new PriorityQueue<>((n1,n2)->n1.getValue()-n2.getValue());
+        
+        
+        for(Map.Entry<Integer,Integer> mp:freq.entrySet())
+        {
+            Pair<Integer,Integer> p = new Pair(mp.getKey(),mp.getValue());
+            minheap.add(p);
+            if(minheap.size()>k)
+                minheap.poll();
+        }
+        
+        // System.out.print(minheap);
+        
+        int result[] = new int[k];
+        int i=0;
+        
+        for(Pair<Integer,Integer> e:minheap)
+        {
+            result[i] = e.getKey();
+            i++;
+        }
+        //Also can use this loop instead
+        // while(minheap.size()>0)
+        // {
+        //     result[i] = minheap.poll().getKey();
+        //     i++;
+        // }
+        
+        
+        
+        
+        
+        
+        return result;
+    }
+}
