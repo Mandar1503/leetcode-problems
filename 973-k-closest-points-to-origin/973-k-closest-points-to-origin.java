79a9aa1a01b0c775d5edb32c06decef7b9123e66
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-10 01:23:35 -0400

    Time: 32 ms (76.17%), Space: 50.4 MB (94.58%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,30 +1,75 @@
+//MY SOLUTION
+// class Solution {
+//     public int[][] kClosest(int[][] points, int k) {
+        
+//         PriorityQueue<Pair<Integer,Integer>> maxh =  new PriorityQueue<>((a,b)->b.getKey()-a.getKey());
+        
+//         for(int i=0;i<points.length;i++)
+//         {
+//             Pair<Integer,Integer> entry = new Pair<>(squaredDist(points[i]),i);
+//             maxh.add(entry);
+//             if(maxh.size()>k)
+//                 maxh.poll();  
+//         }
+        
+//         int result[][] = new int[k][2];
+//         int i=0;
+//         while(maxh.size()>0)
+//         {
+//             int l = maxh.poll().getValue();
+//             result[i][0] = points[l][0];
+//             result[i][1] = points[l][1];
+//             i++;
+//         }
+//         return result;
+//     }
+    
+//     private int squaredDist(int[] point)
+//     {
+//         return point[0]*point[0]+point[1]*point[1];
+//     }
+// }
+
+
+
+
+//A BIT MORE OPTIMIZED SOLUTION
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         
-        PriorityQueue<Pair<Integer,Integer>> maxh =  new PriorityQueue<>((a,b)->b.getKey()-a.getKey());
+        PriorityQueue<Pair<Integer,Integer>> maxheap = new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
         
         for(int i=0;i<points.length;i++)
         {
-            Pair<Integer,Integer> entry = new Pair<>(squaredDist(points[i]),i);
-            maxh.add(entry);
-            if(maxh.size()>k)
-                maxh.poll();  
+            int dist = squaredDist(points[i]);
+            Pair<Integer,Integer> coord = new Pair<>(i,dist);
+            
+            if(maxheap.size()<k)
+            {
+                maxheap.add(coord);
+            }
+            else if(maxheap.peek().getValue()>dist)
+            {
+                maxheap.poll();
+                maxheap.add(coord);
+            }
         }
         
-        int result[][] = new int[k][2];
-        int i=0;
-        while(maxh.size()>0)
+        int[][] result = new int[k][2];
+        int i = 0;
+        while(maxheap.size()>0)
         {
-            int l = maxh.poll().getValue();
+            int l = maxheap.poll().getKey();
             result[i][0] = points[l][0];
             result[i][1] = points[l][1];
             i++;
         }
+        
         return result;
     }
     
     private int squaredDist(int[] point)
     {
-        return point[0]*point[0]+point[1]*point[1];
+        return point[0]*point[0] + point[1]*point[1];
     }
 }
