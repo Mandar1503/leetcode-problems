7f3a027ab1e5ee19fff5333d4ac8929d0da0d4ad
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 19:29:17 -0400

    Time: 4 ms (34.94%), Space: 48.2 MB (18.11%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -56,35 +56,68 @@
 // }
 
 //MY OPTIMIZED SOLUTION USING BFS(100%)
+// class Solution {
+//     public List<Double> averageOfLevels(TreeNode root) {
+//         List<List<Integer>> levels = new LinkedList<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+//         List<Double> result = new LinkedList<>();
+//         int level =0;
+        
+//         if(root==null)
+//             return result;
+        
+//         q.add(root);
+        
+//         while(!q.isEmpty())
+//         {
+//             int qsize = q.size();
+//             double sum =0;
+//             for(int i=0;i<qsize;i++)
+//             {
+//                 TreeNode node =q.remove();   
+//                 sum += node.val;
+        
+//                 if(node.left!=null)
+//                     q.add(node.left);
+//                 if(node.right!=null)
+//                     q.add(node.right);
+//             }
+//             result.add((double)sum/qsize);
+//             level++;
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        List<List<Integer>> levels = new LinkedList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        List<Double> result = new LinkedList<>();
-        int level =0;
+        LinkedList<Integer> count = new LinkedList<>();
+        LinkedList<Double> res = new LinkedList<>();
         
+        average(root,res,0,count);
+        
+        for(int i=0;i<res.size();i++)
+            res.set(i,res.get(i)/count.get(i));
+        return res;
+    }
+    
+    private void average(TreeNode root, LinkedList<Double> res, int i, LinkedList<Integer> count)
+    {
         if(root==null)
-            return result;
-        
-        q.add(root);
-        
-        while(!q.isEmpty())
+            return;
+        if(i<res.size())
         {
-            int qsize = q.size();
-            double sum =0;
-            for(int i=0;i<qsize;i++)
-            {
-                TreeNode node =q.remove();   
-                sum += node.val;
-        
-                if(node.left!=null)
-                    q.add(node.left);
-                if(node.right!=null)
-                    q.add(node.right);
-            }
-            result.add((double)sum/qsize);
-            level++;
+            res.set(i,res.get(i)+root.val);
+            count.set(i,count.get(i)+1);    
         }
-        return result;
+        else
+        {
+            res.add(1.0*root.val);
+            count.add(1);
+        }
+        
+        
+        average(root.left,res,i+1,count);
+        average(root.right,res,i+1,count);
     }
 }
