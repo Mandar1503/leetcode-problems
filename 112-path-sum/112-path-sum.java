2b75c246c00ac67ec3c0a27129412e34e9de47ff
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 17:13:32 -0400

    Time: 0 ms (100.00%), Space: 42.4 MB (82.10%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -15,39 +15,39 @@
  */
 
 //MY SOLUTION = RECURSION = TOP DOWN APPROACH
-// class Solution {
-//     public boolean hasPathSum(TreeNode root, int targetSum) {
-//         if(root==null)
-//             return false;
-        
-//         int sum = 0;
-//         return pathSum(root,sum,targetSum);
-//     }
-    
-//     private boolean pathSum(TreeNode root, int sum, int targetSum)
-//     {
-//         if(root==null)
-//             return false; 
-//         if(root.left==null && root.right==null && (sum+root.val)==targetSum)
-//             return true;
-//         else if(root.left==null && root.right==null && (sum+root.val)!=targetSum)
-//             return false;
-        
-//         return pathSum(root.left, sum+root.val, targetSum) || pathSum(root.right, sum+root.val, targetSum);
-//     }
-// }
-
 class Solution {
-    private boolean res = false;
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        
         if(root==null)
             return false;
         
-        targetSum-=root.val;
-        if(root.left==null && root.right==null && targetSum==0)
+        int sum = 0;
+        return pathSum(root,sum,targetSum);
+    }
+    
+    private boolean pathSum(TreeNode root, int sum, int targetSum)
+    {
+        if(root==null)
+            return false; 
+        if(root.left==null && root.right==null && (sum+root.val)==targetSum)
             return true;
+        else if(root.left==null && root.right==null && (sum+root.val)!=targetSum)
+            return false;
         
-        return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
-    }  
+        return pathSum(root.left, sum+root.val, targetSum) || pathSum(root.right, sum+root.val, targetSum);
+    }
 }
+
+// class Solution {
+//     private boolean res = false;
+//     public boolean hasPathSum(TreeNode root, int targetSum) {
+        
+//         if(root==null)
+//             return false;
+        
+//         targetSum-=root.val;
+//         if(root.left==null && root.right==null && targetSum==0)
+//             return true;
+        
+//         return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
+//     }  
+// }
