c40e72dc4f82f33213f44bf1cae033f6ef3abf09
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 03:39:21 -0400

    Time: 1 ms (55.25%), Space: 43.1 MB (5.46%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -13,33 +13,48 @@
  *     }
  * }
  */
-
 //ITERATIVE SOLUTION
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         Queue<TreeNode> q = new LinkedList<>();
+        
+//         q.add(root);
+//         q.add(root);
+        
+//         while(!q.isEmpty())
+//         {
+//             TreeNode t1 = q.remove();
+//             TreeNode t2 = q.remove();
+            
+//             if(t1==null && t2==null)
+//                 continue;
+//             if(t1==null || t2==null)
+//                 return false;
+            
+//             if(t1.val!=t2.val)
+//                 return false;
+            
+//                 q.add(t1.left);
+//                 q.add(t2.right);
+//                 q.add(t1.right);
+//                 q.add(t2.left);
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        
-        q.add(root);
-        q.add(root);
-        
-        while(!q.isEmpty())
-        {
-            TreeNode t1 = q.remove();
-            TreeNode t2 = q.remove();
-            
-            if(t1==null && t2==null)
-                continue;
-            if(t1==null || t2==null)
-                return false;
-            
-            if(t1.val!=t2.val)
-                return false;
-            
-                q.add(t1.left);
-                q.add(t2.right);
-                q.add(t1.right);
-                q.add(t2.left);
-        }
-        return true;
+        return symmetric(root,root);
+    }
+    
+    private boolean symmetric(TreeNode t1,TreeNode t2)
+    {
+        if(t1==null && t2==null)
+            return true;
+        if(t1==null || t2==null)
+            return false;
+
+        return (t1.val==t2.val) && symmetric(t1.right,t2.left) && symmetric(t1.left,t2.right);
     }
 }
