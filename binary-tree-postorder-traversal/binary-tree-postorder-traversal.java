a23580fe8c5fa357bb91a35c17f040b4e74a0207
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 00:43:56 -0400

    Time: 1 ms, Memory: 42.5 MB - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,20 +15,50 @@
  */
 
 //RECURSIVE APPROACH
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+        
+//         ArrayList<Integer> l = new ArrayList<>();
+//         postorder(root, l);
+//         return l;
+//     }
+    
+//     private void postorder(TreeNode root, ArrayList<Integer> l)
+//     {
+//         if(root==null)
+//             return;
+//         postorder(root.left,l);
+//         postorder(root.right,l);
+//         l.add(root.val);
+//     }
+// }
+
+//RECURSIVE APPROACH  = SAME AS PREORDER JUST ADDFIRST IN THE ANS AND PUSH LEFT AND THEN RIGHT INTO THE STACK.
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         
-        ArrayList<Integer> l = new ArrayList<>();
-        postorder(root, l);
-        return l;
-    }
-    
-    private void postorder(TreeNode root, ArrayList<Integer> l)
-    {
+        LinkedList<Integer> l = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        
         if(root==null)
-            return;
-        postorder(root.left,l);
-        postorder(root.right,l);
-        l.add(root.val);
+            return l;
+        
+        stack.push(root);                   //PUSH ROOT INTO STACK
+        
+        while(!stack.empty())
+        {
+            TreeNode node = stack.pop();    //POP AND ADDFIRST IN ANS
+            l.addFirst(node.val);
+            
+            if(node.left!=null)             //LEFT
+            {
+                stack.push(node.left);
+            }
+            if(node.right!=null)            //RIGHT
+            {
+                stack.push(node.right);
+            }
+        }
+        return l;
     }
 }
