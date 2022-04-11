7e773ad41394daf8e9987b29e287ab6731708e81
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 17:29:05 -0400

    Time: 4 ms (5.14%), Space: 44.5 MB (7.95%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -15,28 +15,29 @@
  */
 
 //MY SOLUTION = RECURSION = TOP DOWN APPROACH
-class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null)
-            return false;
+// class Solution {
+//     public boolean hasPathSum(TreeNode root, int targetSum) {
+//         if(root==null)
+//             return false;
         
-        int sum = 0;
-        return pathSum(root,sum,targetSum);
-    }
+//         int sum = 0;
+//         return pathSum(root,sum,targetSum);
+//     }
     
-    private boolean pathSum(TreeNode root, int sum, int targetSum)
-    {
-        if(root==null)
-            return false; 
-        if(root.left==null && root.right==null && (sum+root.val)==targetSum)
-            return true;
-        // else if(root.left==null && root.right==null && (sum+root.val)!=targetSum)
-        //     return false;
+//     private boolean pathSum(TreeNode root, int sum, int targetSum)
+//     {
+//         if(root==null)
+//             return false; 
+//         if(root.left==null && root.right==null && (sum+root.val)==targetSum)
+//             return true;
+//     // else if(root.left==null && root.right==null && (sum+root.val)!=targetSum)//it works even if this is removed
+//     //     return false;
         
-        return pathSum(root.left, sum+root.val, targetSum) || pathSum(root.right, sum+root.val, targetSum);
-    }
-}
+//         return pathSum(root.left, sum+root.val, targetSum) || pathSum(root.right, sum+root.val, targetSum);
+//     }
+// }
 
+//Same as above(TOP DOWN APPROACH)
 // class Solution {
 //     private boolean res = false;
 //     public boolean hasPathSum(TreeNode root, int targetSum) {
@@ -51,3 +52,39 @@ class Solution {
 //         return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
 //     }  
 // }
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> l = new Stack<>();
+        
+        if(root==null)
+            return false;
+        
+        stack.push(root);
+        l.push(targetSum-root.val);
+        
+        TreeNode curr;
+        int curr_sum;
+        while(!stack.isEmpty())
+        {
+            curr = stack.pop();
+            curr_sum = l.pop();
+            
+            if(curr.right==null && curr.left==null && curr_sum==0)
+                return true;
+            
+            if(curr.right!=null)
+            {
+                stack.push(curr.right);
+                l.push(curr_sum-curr.right.val);
+            }
+             if(curr.left!=null)
+            {
+                stack.push(curr.left);
+                l.push(curr_sum-curr.left.val);
+            }
+        }
+        return false;
+    } 
+}
