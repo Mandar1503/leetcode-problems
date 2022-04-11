319818218f9b12f90a537c3f3ca91916eb61b835
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 00:19:16 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (52.55%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,20 +15,43 @@
  */
 
 //RECURSIVE SOLUTION
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         ArrayList<Integer> l = new ArrayList<>();
+//         inorder(root,l);
+//         return l;
+//     }
+    
+//     private void inorder(TreeNode root,ArrayList<Integer> l)
+//     {
+//         if(root==null)
+//             return;
+        
+//         inorder(root.left,l);
+//         l.add(root.val);
+//         inorder(root.right,l);
+//     }
+// }
+
+//ITERATIVE APPROACH (FIRST ASSIGN CURR, GO TO LEFT END WHILE ADDING INTO STACK, THEN POP AND PRINT,  GO RIGHT)
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> l = new ArrayList<>();
-        inorder(root,l);
-        return l;
-    }
-    
-    private void inorder(TreeNode root,ArrayList<Integer> l)
-    {
-        if(root==null)
-            return;
+        Stack<TreeNode> stack = new Stack<>();
         
-        inorder(root.left,l);
-        l.add(root.val);
-        inorder(root.right,l);
+        TreeNode curr = root;
+        
+        while(curr!=null || !stack.isEmpty())
+        {
+            while(curr!=null)
+            {
+                stack.add(curr);
+                curr=curr.left;
+            }
+            curr=stack.pop();
+            l.add(curr.val);
+            curr=curr.right;
+        }
+        return l;
     }
 }
