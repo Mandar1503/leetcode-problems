cac002b910043c1de57fdad0d1614c4d98fab117
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 16:05:52 -0400

    Time: 0 ms (100.00%), Space: 43.4 MB (30.59%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -15,26 +15,28 @@
  */
 
 //TOP DOWN APPROACH BUT NOT WORKING
-// class Solution {
-//     private int answer = 1;
-//     public int maxDepth(TreeNode root) {
-//         int depth = 1;
-//         maxD(root,depth);
-//         return answer;
-//     }
-//     void maxD(TreeNode root,int depth)
-//     {
-//         if(root==null)
-//             return;
+class Solution {
+    private int answer = 1;
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        int depth = 1;
+        maxD(root,depth);
+        return answer;
+    }
+    void maxD(TreeNode root,int depth)
+    {
+        if(root==null)
+            return;
         
-//         if(root.left==null && root.right==null)
-//             answer = Math.max(answer,depth);
+        if(root.left==null && root.right==null)
+            answer = Math.max(answer,depth);
         
-//         maxD(root.left,depth+1);
-//         maxD(root.right,depth+1);
-//         return;
-//     }
-// }
+        maxD(root.left,depth+1);
+        maxD(root.right,depth+1);
+        return;
+    }
+}
 
 
 //RECURSION = BOTTOM UP APPROACH 
@@ -53,31 +55,31 @@
 
 
 //ITERATIVE USE BFS
-class Solution {
-    public int maxDepth(TreeNode root) {
+// class Solution {
+//     public int maxDepth(TreeNode root) {
         
-        Queue<TreeNode> q = new LinkedList<>();
-        int level =0;
+//         Queue<TreeNode> q = new LinkedList<>();
+//         int level =0;
         
-        if(root==null)
-            return 0;
+//         if(root==null)
+//             return 0;
         
-        q.add(root);
+//         q.add(root);
         
-        while(!q.isEmpty())
-        {
-            int qsize =q.size(); //IMPO
+//         while(!q.isEmpty())
+//         {
+//             int qsize =q.size(); //IMPO
             
-            for(int i=0;i<qsize;i++)
-            {
-                TreeNode node = q.remove();
-                if(node.left!=null)
-                    q.add(node.left);
-                if(node.right!=null)
-                    q.add(node.right);
-            }
-            level++;
-        }
-        return level;
-    }
-}
+//             for(int i=0;i<qsize;i++)
+//             {
+//                 TreeNode node = q.remove();
+//                 if(node.left!=null)
+//                     q.add(node.left);
+//                 if(node.right!=null)
+//                     q.add(node.right);
+//             }
+//             level++;
+//         }
+//         return level;
+//     }
+// }
