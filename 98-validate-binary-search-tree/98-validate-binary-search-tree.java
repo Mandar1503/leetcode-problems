be2b1fc62e5fb3f1a2382167cc2c057b0dfee445
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 10:42:19 -0400

    Time: 3 ms (12.88%), Space: 43 MB (80.27%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -34,21 +34,52 @@
 // }
 
 //RECURSIVE APPROACH USING INORDER
-class Solution {
+// class Solution {
     
-    Integer prev = null;
+//     Integer prev = null;
     
-    public boolean isValidBST(TreeNode root) {
+//     public boolean isValidBST(TreeNode root) {
         
+//         if(root==null)
+//             return true;
+        
+//         if(!isValidBST(root.left))
+//             return false;
+        
+//         if(prev!=null && root.val<=prev)
+//             return false;
+//         prev = root.val;
+        
+//         return isValidBST(root.right);
+        
+//     }
+// }
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
         if(root==null)
             return true;
         
-        if(!isValidBST(root.left))
-            return false;
-        if(prev!=null && root.val<=prev)
-            return false;
-        prev = root.val;
-        return isValidBST(root.right);
+        Stack<TreeNode> stack = new Stack<>();
         
+        TreeNode prev = null;
+        TreeNode curr=root;
+        
+        while(curr!=null || !stack.isEmpty())
+        {
+            while(curr!=null)
+            {
+                stack.push(curr);
+                curr=curr.left;
+            }
+            curr = stack.pop();
+            if(prev!=null && curr.val<=prev.val)
+            {
+                return false;                
+            }
+            prev = curr;
+            curr=curr.right;
+        }
+        return true;
     }
 }
