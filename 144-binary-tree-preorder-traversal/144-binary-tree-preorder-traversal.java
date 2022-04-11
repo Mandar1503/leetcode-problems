76c4681e6dc9093a30271fa4fc43555bd76bac08
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-10 23:40:40 -0400

    Time: 1 ms (44.09%), Space: 42.5 MB (34.75%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -13,49 +13,53 @@
  *     }
  * }
  */
+
+//RECURSIVE SOLUTION
 // class Solution {
 //     public List<Integer> preorderTraversal(TreeNode root) {
-//         Stack<TreeNode> stack = new Stack<>();// LinkedList<TreeNode> stack = new LinkedList<>();
-//         LinkedList<Integer> output = new LinkedList<>();
         
+//         LinkedList<Integer> l = new LinkedList<>();
+//         preorder(root,l);
+//         return l;
+//     }
+//     private void preorder(TreeNode root, LinkedList<Integer> l)
+//     {
 //         if(root==null)
-//             return output;
-        
-//         stack.add(root);
-        
-//         while(!stack.isEmpty())
-//         {
-//             TreeNode node = stack.pop();//TreeNode node = stack.pollLast();
-//             output.add(node.val);
-            
-//             if(node.right!=null)
-//             {
-//                 stack.push(node.right);//stack.add(node.right);
-//             }
-//             if(node.left!=null)
-//             {
-//                 stack.push(node.left);//stack.add(node.left);
-//             }
-//         }
-        
-//         return output;
+//             return; 
+//         l.add(root.val);
+//         preorder(root.left,l);
+//         preorder(root.right,l);
 //     }
 // }
 
-//RECURSIVE SOLUTION
+
+//ITERATIVE APPROACH
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();// LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
         
-        LinkedList<Integer> l = new LinkedList<>();
-        preorder(root,l);
-        return l;
-    }
-    private void preorder(TreeNode root, LinkedList<Integer> l)
-    {
         if(root==null)
-            return; 
-        l.add(root.val);
-        preorder(root.left,l);
-        preorder(root.right,l);
+            return output;
+        
+        stack.add(root);
+        
+        while(!stack.isEmpty())
+        {
+            TreeNode node = stack.pop();//TreeNode node = stack.pollLast();
+            output.add(node.val);
+            
+            if(node.right!=null)
+            {
+                stack.push(node.right);//stack.add(node.right);
+            }
+            if(node.left!=null)
+            {
+                stack.push(node.left);//stack.add(node.left);
+            }
+        }
+        
+        return output;
     }
 }
+
