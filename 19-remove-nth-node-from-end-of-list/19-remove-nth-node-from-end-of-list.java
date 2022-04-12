bcfbca8aabe8640a8e1d936b7536418789ebb9ea
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-12 01:08:38 -0400

    Time: 0 ms (100.00%), Space: 42.7 MB (20.29%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -9,7 +9,7 @@
  * }
  */
 
-//MY SOLUTION  TWO POINTER AND IMPO TRICK(SET NODE TO HEAD WHEN LLENGTH=N)
+//MY SOLUTION  TWO POINTER AND IMPO TRICK(SET NODE TO HEAD WHEN LLENGTH=N) FOR ONE MORE IMPO POINT LOOK THE COMMENT IN CODE
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode tempNode = head;
@@ -23,13 +23,13 @@ class Solution {
             if(endNode!=null)
                 endNode=endNode.next;
             
-            if(listlength==n)
+            if(listlength==n)       
                 endNode = head;
             
             tempNode=tempNode.next;
         }
         
-        if(endNode==null)
+        if(endNode==null)   //VERY VERY IMPO STEP => IF N == TOTALLENGTH OF LIST, THEN HEAD IS TO BE REMOVERED SINCE HEAD IS THE NTH NODE FROM THE END
             return head.next;
         
         ListNode node = endNode.next;
