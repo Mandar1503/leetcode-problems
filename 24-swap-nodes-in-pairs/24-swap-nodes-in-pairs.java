d28b0d5177bcd0ff7641a6a56e4badc76df0e091
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-11 23:22:05 -0400

    Time: 0 ms (100.00%), Space: 42 MB (39.38%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -20,6 +20,7 @@
         
 //             node1.next = swapPairs(head.next.next); //SWAP
 //             node2.next = node1;
+
 //             return node2;                           //NEW HEAD SHOULD BE THE HEAD OF THIS SUBLIST
             
 //     }
@@ -28,23 +29,24 @@
 
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode dummy = new ListNode(-1);
+        
+        ListNode dummy = new ListNode(-1); //DUMMY HEAD TO KEEP TRACK OF THE HEAD AND TO ASSIGN PREVNODE TO IT INITIALLY
         dummy.next = head;
         
         ListNode prevNode = dummy;
         
-        while(head!=null && head.next!=null)
+        while(head!=null && head.next!=null)    //WRITE BOTH HEAD & HEAD.NEXT
         {
-            ListNode N1 = head;
+            ListNode N1 = head;                 //ASSIGN N1 & N2
             ListNode N2 = head.next;
             
-            N1.next = N2.next;
+            N1.next = N2.next;                  //SWAPPING
             N2.next = N1;
             prevNode.next = N2;
             
-            prevNode = N1;
+            prevNode = N1;                      //REASSIGNING THE PREVNODE AND THE HEAD FOR NEXT ITERATION
             head = N1.next;
         }
-        return dummy.next;
+        return dummy.next;                      //RETURN DUMMY.NEXT WHICH IS HEAD
     }
 }
