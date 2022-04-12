df7a7faec21eb9087a994f37a9fe320fddc42d1f
Mandar1503 <79890138+Mandar1503@users.noreply.github.com>
2022-04-12 12:13:22 -0400

    Time: 16 ms (33.55%), Space: 49.3 MB (48.13%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.java
+++ 696-count-binary-substrings/696-count-binary-substrings.java
@@ -1,27 +1,51 @@
+//GROUP BY CHARACTER
+// class Solution {
+//     public int countBinarySubstrings(String s) {
+        
+//         int group[] = new int[s.length()];
+//         int t=0;
+//         group[0]=1;
+//         for(int i=1;i<s.length();i++)
+//         {
+//             if(s.charAt(i-1)!=s.charAt(i))
+//             {
+//                 t++;
+//                 group[t] = 1;
+//             }
+//             else
+//             {
+//                 group[t]++;
+//             }
+//         }
+        
+//         int ans =0;
+//         for(int i=1;i<=t;i++)
+//         {
+//             ans+=Math.min(group[i-1],group[i]);
+//         }
+//         return ans;
+//     }
+// }
+
+//LINEAR SCAN
 class Solution {
     public int countBinarySubstrings(String s) {
+        int prev=0,curr=1,ans=0;
         
-        int group[] = new int[s.length()];
-        int t=0;
-        group[0]=1;
         for(int i=1;i<s.length();i++)
         {
-            if(s.charAt(i-1)!=s.charAt(i))
+            if(s.charAt(i)!=s.charAt(i-1))
             {
-                t++;
-                group[t] = 1;
+                ans+=Math.min(prev,curr);
+                prev = curr;
+                curr =1;
             }
             else
             {
-                group[t]++;
+                curr++;
             }
         }
         
-        int ans =0;
-        for(int i=1;i<=t;i++)
-        {
-            ans+=Math.min(group[i-1],group[i]);
-        }
-        return ans;
+        return ans+Math.min(prev,curr);
     }
 }
