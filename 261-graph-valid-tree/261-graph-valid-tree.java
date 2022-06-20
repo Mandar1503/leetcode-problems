b9423e4771545f7350b54da660d070f84ab5d68d
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2022-06-20 15:52:03 -0400

    Time: 1 ms (90.06%), Space: 46.8 MB (49.38%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,7 +1,83 @@
+//MY SOLUTION(USING COUNT FUNCTION)
+// class UnionFind{
+//     int[] root;
+//     int[] rank;
+    
+//     public UnionFind(int length)
+//     {
+//         root = new int[length];
+//         rank = new int[length];
+        
+//         for(int i=0;i<length;i++)
+//         {
+//             rank[i] = 1;
+//             root[i] = i;
+//         }
+//     }
+    
+//     public int find(int x)
+//     {
+//         if(root[x]==x)
+//             return x;
+//         return root[x] = find(root[x]);
+//     }
+    
+//     public void union(int x, int y)
+//     {
+//         int rootX = find(x);
+//         int rootY = find(y);
+        
+//         if(rootX!=rootY)
+//         {
+//             if(rank[rootX] > rank[rootY])
+//                 root[rootY] = rootX;
+//             else if(rank[rootX] < rank[rootY])
+//                 root[rootX] = rootY;
+//             else
+//             {
+//                 root[rootY] = rootX;
+//                 rootX+=1;
+//             }
+//         }
+//     }
+    
+//     public boolean count()
+//     {
+//         Set<Integer> set = new HashSet<>();
+//         for(int i=0;i<root.length;i++)
+//         {
+//             set.add(find(i));                //important step in the code 
+//         }
+//         return set.size()==1?true:false;
+//     }
+// }
+
+
+// class Solution {
+//     public boolean validTree(int n, int[][] edges) {
+//         UnionFind uf = new UnionFind(n);
+
+//         int count = 0;
+//         for(int i=0;i<edges.length;i++)
+//         {
+//             uf.union(edges[i][0],edges[i][1]);
+//             count++;
+//         }
+       
+//         if(count == n-1 && uf.count())
+//             return true;
+//         else 
+//             return false;
+//     }
+// }
+
+
+//THEIR SOLUTION(BY MODIFYING THE UNION FUNCTION)
 class UnionFind{
     int[] root;
     int[] rank;
     
+    //initialization
     public UnionFind(int length)
     {
         root = new int[length];
@@ -14,6 +90,7 @@ class UnionFind{
         }
     }
     
+    //find function with path optimization
     public int find(int x)
     {
         if(root[x]==x)
@@ -21,10 +98,14 @@ class UnionFind{
         return root[x] = find(root[x]);
     }
     
-    public void union(int x, int y)
+    //slightly changed union function return false if root is same before union otherwise true
+    public boolean union(int x, int y)
     {
         int rootX = find(x);
         int rootY = find(y);
+        
+        if(rootX==rootY)
+            return false;
         
         if(rootX!=rootY)
         {
@@ -38,16 +119,7 @@ class UnionFind{
                 rootX+=1;
             }
         }
-    }
-    
-    public boolean count()
-    {
-        Set<Integer> set = new HashSet<>();
-        for(int i=0;i<root.length;i++)
-        {
-            set.add(find(i));                //important step in the code 
-        }
-        return set.size()==1?true:false;
+        return true;
     }
 }
 
@@ -55,17 +127,16 @@ class UnionFind{
 class Solution {
     public boolean validTree(int n, int[][] edges) {
         UnionFind uf = new UnionFind(n);
-
-        int count = 0;
+       
+        if(edges.length!=n-1)
+            return false;
+        
         for(int i=0;i<edges.length;i++)
         {
-            uf.union(edges[i][0],edges[i][1]);
-            count++;
+            if(!uf.union(edges[i][0],edges[i][1]))
+                return false;
         }
-       
-        if(count == n-1 && uf.count())
-            return true;
-        else 
-            return false;
+        
+        return true;
     }
 }
