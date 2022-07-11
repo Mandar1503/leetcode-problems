e9fc1548022818992327f974298529390625fe5f
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2022-07-11 03:53:27 -0400

    Time: 917 ms (23.78%), Space: 117.7 MB (43.10%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,3 +1,109 @@
+//THIS CODE DOESN'T WORK SINCE IT USES MAPS TO STORE THE DIST AND THE POINTS, IF THERE ARE MULTIPLE SAME DISTANCE THEN A MAP DOESN'T WORK IN THIS CASE
+// class UnionFind
+// {
+//     int[] root;
+//     int[] rank;
+    
+//     public UnionFind(int length)
+//     {
+//         root = new int[length];
+//         rank = new int[length];
+        
+//         for(int i=0;i<length;i++)
+//         {
+//             root[i] = i;
+//             rank[i] = 1;
+//         }
+//     }
+    
+//     public int find(int x)
+//     {
+//         if(root[x] == x)
+//             return x;
+        
+//         return root[x] = find(root[x]);
+//     }
+    
+//     public void union(int x, int y)
+//     {
+//         int rootX = find(x);
+//         int rootY = find(y);
+        
+//         if(rootX == rootY)
+//             return;
+//         else
+//         {
+//             if(rank[rootX] > rank[rootY])
+//                 root[rootY] = rootX;
+//             else if(rank[rootX] < rank[rootY])
+//                 root[rootX] = rootY;
+//             else if(rank[rootX] == rank[rootY])
+//             {
+//                 root[rootY] = rootX;
+//                 rank[rootX]+=1;
+//             }
+//         }
+//     }
+    
+//     public boolean connected(int x, int y)
+//     {
+//         return find(x)==find(y);
+//     }
+// }
+
+
+// class Solution {
+    
+//     public int manhattanDist(int x1, int y1, int x2, int y2)
+//     {
+//         return Math.abs(x2-x1)+Math.abs(y2-y1);
+//     }
+    
+//     public static TreeMap<Integer,ArrayList<Integer>> sortByKey(Map<Integer,ArrayList<Integer>> map1)
+//     {
+//         TreeMap<Integer,ArrayList<Integer>> sorted = new TreeMap<>(map1);
+        
+//         for(Map.Entry<Integer,ArrayList<Integer>> entry : sorted.entrySet())
+//         {
+//             System.out.println("Key ="+ entry.getKey()+" , points = "+ entry.getValue().get(0) +" , "+ entry.getValue().get(1));
+//         }
+//         return sorted;
+//     }
+    
+//     public int minCostConnectPoints(int[][] points) {
+        
+//         int n = points.length;
+//         UnionFind uf = new UnionFind(n);
+//         Map<Integer,ArrayList<Integer>> distbwpoints = new HashMap<>();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 int dist = manhattanDist(points[i][0],points[i][1],points[j][0],points[j][1]);
+//                 distbwpoints.put(dist,new ArrayList<Integer>(Arrays.asList(i,j)));
+//             }
+//         }
+//         Map<Integer,ArrayList<Integer>> pointsSorted = sortByKey(distbwpoints);
+        
+//         int mstCost = 0;
+//         int edgesUsed = 0;
+        
+//         for(Map.Entry<Integer,ArrayList<Integer>> entry: pointsSorted.entrySet())
+//         {
+//             if(!uf.connected(entry.getValue().get(0),entry.getValue().get(1)))
+//             {
+//                 uf.union(entry.getValue().get(0),entry.getValue().get(1));
+//                 mstCost += entry.getKey();
+//                 edgesUsed += 1;
+//             } 
+//         }
+//         return mstCost;
+//     }
+// }
+
+
+//THIS IS CORRECT SOLUTION USING KRUSKALS ALGORITHM 
 class UnionFind
 {
     int[] root;
@@ -52,14 +158,6 @@ class UnionFind
 
 class Solution {
     
-    //Map<Integer,ArrayList<Integer>> distbwpoints = new HashMap<>();
-    
-//     public static void sortByKey()
-//     {
-//         TreeMap<Integer,ArrayList<Integer>> sorted = new TreeMap<>(distbwpoints);
-//         return sorted;
-//     }
-    
     public int minCostConnectPoints(int[][] points) { 
         
         int n = points.length;
@@ -71,13 +169,17 @@ class Solution {
             for(int j=i+1;j<n;j++)
             {
                 int dist = manhattanDist(points[i][0],points[i][1],points[j][0],points[j][1]);
-                //distbwpoints.put(dist,new ArrayList<Integer>(Arrays.asList(i,j)));
                 int[] currEdge = {dist,i,j};
                 allEdges.add(currEdge);
             }
         }
         
         Collections.sort(allEdges, (a,b)->Integer.compare(a[0],b[0]));
+        
+        // for(int i = 0; i < allEdges.size(); i++)
+        // {
+        //     System.out.println("Dist = "+allEdges.get(i)[0]+", points = "+allEdges.get(i)[1]+" , "+allEdges.get(i)[2]);
+        // }
         
         int mstCost = 0;
         int edgesUsed = 0;
@@ -94,7 +196,6 @@ class Solution {
                 edgesUsed+=1;
             }
         }
-        //Map<Integer,ArrayList<Integer>> sortedByDist = sortByKey();
         return mstCost;        
     }
     
