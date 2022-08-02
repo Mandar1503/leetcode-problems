1f8b49c9a3c7d962d9d626e3a3d952d7c671ce51
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2022-08-02 03:33:56 -0400

    Time: 0 ms (100.00%), Space: 43 MB (19.45%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,37 +1,38 @@
 class Solution {
     public int findMin(int[] nums) {
         
-        if(nums.length==1)
+        //if length if the nums is 1 then return the 1st element 
+        if(nums.length == 1)
             return nums[0];
         
+        //set left and right index to the 1st and the last element index
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length -1;
         
-        if(nums[right]>nums[0])
+        //if 1st index element is smaller than the last element then the array is sorted and return 1st element  
+        if(nums[0]<nums[right])
             return nums[0];
         
-        while(left<=right)
+        //while left <= right
+        while(left <= right)
         {
-            int mid = left + (right-left)/2;
+            int mid = left + (right - left)/2;
             
+            //if mid is > than mid + 1, then mid + 1 is the smallest element, since it is the point of inflection
             if(nums[mid] > nums[mid+1])
-            {
                 return nums[mid+1];
-            }
-            else if(nums[mid-1] > nums[mid])
-            {
+            else if(nums[mid-1] > nums[mid]) //here mid is the point of inflection
                 return nums[mid];
-            }
             
-            if(nums[0] < nums[mid])
+            if(nums[0] < nums[mid]) //if num at 0 is smaller than num at mid then search for the inflection point on the right hand side
             {
-                left = mid+1;
+                left = mid + 1;
             }
-            else
+            else //else search at the left hand side 
             {
-                right = mid-1;
+                right = mid - 1;
             }
         }
-        return Integer.MAX_VALUE;
+        return Integer.MAX_VALUE;   //else return the max value of the Integer
     }
 }
