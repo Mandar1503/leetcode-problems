cf7e309386b0e86aecf8e483ba633535ac7f583d
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2022-08-21 03:48:27 -0400

    Time: 5 ms (73.18%), Space: 44.1 MB (48.34%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,33 +1,49 @@
 //MY SOLUTION USING HASHMAPS
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> sCharSet = new HashMap<>();
-        HashMap<Character,Integer> tCharSet = new HashMap<>();
-        
-        if(s.length()!=t.length())
-            return false;
-        
-        for(int i=0;i<t.length();i++)
-        {
-            char ch = s.charAt(i);
-            sCharSet.put(ch, sCharSet.getOrDefault(ch,0)+1);
-            char chr = t.charAt(i);
-            tCharSet.put(chr, tCharSet.getOrDefault(chr,0)+1);
-        }
-        
-        for(Map.Entry<Character,Integer> mp : sCharSet.entrySet())
-        {
-            if(!tCharSet.containsKey(mp.getKey()) || tCharSet.get(mp.getKey()).intValue()!=mp.getValue().intValue())
-            {
-                return false;
-            }
-        }
-        return true;
-    } 
-}
-
 // class Solution {
 //     public boolean isAnagram(String s, String t) {
+//         HashMap<Character,Integer> sCharSet = new HashMap<>();
+//         HashMap<Character,Integer> tCharSet = new HashMap<>();
         
-//     }
+//         if(s.length()!=t.length())
+//             return false;
+        
+//         for(int i=0;i<t.length();i++)
+//         {
+//             char ch = s.charAt(i);
+//             sCharSet.put(ch, sCharSet.getOrDefault(ch,0)+1);
+//             char chr = t.charAt(i);
+//             tCharSet.put(chr, tCharSet.getOrDefault(chr,0)+1);
+//         }
+        
+//         for(Map.Entry<Character,Integer> mp : sCharSet.entrySet())
+//         {
+//             if(!tCharSet.containsKey(mp.getKey()) || tCharSet.get(mp.getKey()).intValue()!=mp.getValue().intValue())
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     } 
 // }
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        
+        int[] table  = new int[26];
+        
+        for(int i=0;i<s.length();i++)
+        {
+            table[s.charAt(i) - 'a']++;
+            table[t.charAt(i) - 'a']--;
+        }
+        
+        for(int c:table)
+        {
+            if(c!=0)
+                return false;
+        }
+        return true;
+    }
+}
