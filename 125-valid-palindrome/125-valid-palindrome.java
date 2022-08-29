606cf1e205982d85299765e11caf877037502492
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2022-08-28 23:34:11 -0400

    Time: 13 ms (45.61%), Space: 44.4 MB (44.02%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toUpperCase();
+        int i =0;
+        
+        ArrayList<Character> sarr = new ArrayList<>();
+        for(char c : s.toCharArray())
+        {
+            if(Character.isLetterOrDigit(c))
+                sarr.add(c);
+        }
+        int l=0;
+        int r=sarr.size()-1;
+        
+        while(l <= r)
+        {
+            if(sarr.get(l) == sarr.get(r))
+            {
+                l++;
+                r--;
+            }
+            else
+                return false;
+        }
+        // for(Character c : sarr)
+        // {
+        //     System.out.println(c);
+        // }
+        return true;
+    }
+}
