3cd241363eb19e993c53a27611329e0a6f7f0035
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2022-08-28 22:18:32 -0400

    Time: 2 ms (91.18%), Space: 42.3 MB (33.17%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,45 +1,47 @@
-//Using hashmap
+//Both the solutions are basically the same
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character,Character> map = new HashMap<>();
-        Stack<Character> stk = new Stack<>();
-        map.put(')','(');
-        map.put(']','[');
-        map.put('}','{');
+        Stack<Character> br = new Stack<>();
         
-        for(char c: s.toCharArray())
+        for(char c : s.toCharArray())
         {
-            if(map.containsKey(c))
-            {
-                if(!stk.isEmpty() && map.get(c)==stk.peek())
-                    stk.pop();
-                else
-                    return false;
-            }
+            if(c=='{' || c=='(' || c=='[')
+                br.push(c);
             else
             {
-                stk.push(c);
+                if(br.isEmpty() || (c!='}' && br.peek()=='{') || (c!=')' && br.peek()=='(') ||(c!=']' && br.peek()=='['))
+                    return false;
+                br.pop();
             }
         }
-        return stk.isEmpty();
+        return br.isEmpty();
     }
 }
 
+//Using hashmap
 // class Solution {
 //     public boolean isValid(String s) {
-//         Stack<Character> br = new Stack<>();
+//         HashMap<Character,Character> map = new HashMap<>();
+//         Stack<Character> stk = new Stack<>();
+//         map.put(')','(');
+//         map.put(']','[');
+//         map.put('}','{');
         
-//         for(char c : s.toCharArray())
+//         for(char c: s.toCharArray())
 //         {
-//             if(c=='{' || c=='(' || c=='[')
-//                 br.push(c);
+//             if(map.containsKey(c))
+//             {
+//                 if(!stk.isEmpty() && map.get(c)==stk.peek())
+//                     stk.pop();
+//                 else
+//                     return false;
+//             }
 //             else
 //             {
-//                 if(br.isEmpty() || (c!='}' && br.peek()=='{') || (c!=')' && br.peek()=='(') ||(c!=']' && br.peek()=='['))
-//                     return false;
-//                 br.pop();
+//                 stk.push(c);
 //             }
 //         }
-//         return br.isEmpty();
+//         return stk.isEmpty();
 //     }
 // }
+
