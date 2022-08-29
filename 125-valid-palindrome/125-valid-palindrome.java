432ba06dfb562e651aaa3241790a86ce7c291d52
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2022-08-29 00:07:53 -0400

    Time: 7 ms (59.05%), Space: 44.6 MB (41.13%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,28 +1,48 @@
-//MY SOLUTION USING TWO POINTER
+// //MY SOLUTION USING TWO POINTER
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         s = s.toUpperCase();
+//         int i =0;
+        
+//         ArrayList<Character> sarr = new ArrayList<>();
+//         for(char c : s.toCharArray())
+//         {
+//             if(Character.isLetterOrDigit(c))
+//                 sarr.add(c);
+//         }
+//         int l=0;
+//         int r=sarr.size()-1;
+        
+//         while(l <= r)
+//         {
+//             if(sarr.get(l) == sarr.get(r))
+//             {
+//                 l++;
+//                 r--;
+//             }
+//             else
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toUpperCase();
-        int i =0;
+        StringBuilder sb = new StringBuilder();
         
-        ArrayList<Character> sarr = new ArrayList<>();
         for(char c : s.toCharArray())
         {
             if(Character.isLetterOrDigit(c))
-                sarr.add(c);
-        }
-        int l=0;
-        int r=sarr.size()-1;
-        
-        while(l <= r)
-        {
-            if(sarr.get(l) == sarr.get(r))
             {
-                l++;
-                r--;
+                sb.append(Character.toLowerCase(c)); 
             }
-            else
-                return false;
         }
-        return true;
+        
+        String filteredString = sb.toString();
+        String reversedString = sb.reverse().toString();
+        
+        return filteredString.equals(reversedString);
+            
     }
 }
