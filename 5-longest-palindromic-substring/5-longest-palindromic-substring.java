d8c1700961dc50586774dd377b6accb2d5ba889e
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2022-09-03 21:04:47 -0400

    Time: 49 ms (55.15%), Space: 51.9 MB (40.39%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,40 +1,44 @@
-class Solution {
-    public String longestPalindrome(String s) {
-        int reslen = 0;
-        StringBuilder res = new StringBuilder();
-        
-        for(int i=0;i<s.length();i++)
-        {
-            int l = i;
-            int r = i;
+class Solution
+{
+	public String longestPalindrome(String s)
+	{
+		int reslen = 0;
+		StringBuilder res = new StringBuilder();
+		
+		for(int i=0;i<s.length();i++)
+		{
+			int l = i;
+			int r = i;
+			
             // helperfunction(l,r,reslen,res,s);
-            while(l >=0 && r < s.length() && s.charAt(l)==s.charAt(r))
-            {
-                if((r-l+1) > reslen)
-                {
-                    res.delete(0,s.length());
-                    res.append(s.substring(l,r+1));
-                    reslen = r-l+1;   
-                }
-                l--;
-                r++;
-            }
-            
-            l = i;
-            r = i+1;
-            while(l >=0 && r < s.length() && s.charAt(l)==s.charAt(r))
-            {
-                if((r-l+1) > reslen)
-                {
-                    res.delete(0,res.length());
-                    res.append(s.substring(l,r+1));
-                    reslen = r-l+1;   
-                }
-                l--;
-                r++;
-            }
+			while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r))
+			{
+				if(r-l+1 > reslen)
+				{
+					res.delete(0,reslen);
+					res.append(s.substring(l,r+1));
+					reslen = r-l+1;
+				}	
+				l--;
+				r++;
+			}
+			
+			l = i;
+			r = i+1;
+
             // helperfunction(l,r,reslen,res,s);
-        }
-        return res.toString();
-    }
+			while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r))
+			{
+				if(r-l+1 > reslen)
+				{
+					res.delete(0,reslen);
+					res.append(s.substring(l,r+1));
+					reslen = r-l+1;
+				}	
+				l--;
+				r++;
+			}
+		}
+		return res.toString();
+	}
 }
