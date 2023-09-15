c288aea3e32c61acb377b978b1a8fa53d2fc6433
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2023-09-15 19:54:44 -0400

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.java
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -40,6 +40,28 @@ class GFG{
 //User function Template for Java
 
 class Solution{
+    
+    static void dfs(int x, int y, int[][] vis, char[][] a, int[] delrow, int[] delcol)
+    {
+        // int[] delrow = {1, 0, -1, 0};
+        // int[] delcol = {0, 1, 0, -1};
+        
+        vis[x][y] = 1;
+        int r = a.length;
+        int c = a[0].length;
+        
+        for(int i=0;i<4;i++)
+        {
+            int nrow = x + delrow[i];
+            int ncol = y + delcol[i];
+            
+            if(nrow>=0 && nrow<r && ncol>=0 && ncol<c && vis[nrow][ncol]==0 && a[nrow][ncol]=='O')
+            {
+                dfs(nrow, ncol, vis, a, delrow, delcol);
+            }
+        }
+    }
+    
     static char[][] fill(int n, int m, char a[][])
     {
         // code here
@@ -49,12 +71,12 @@ class Solution{
         
         for(int i=0;i<n;i++)
         {
-            if(a[i][0] == 'O' && vis[i][0]!=1)
+            if(a[i][0] == 'O' && vis[i][0]==0)
             {
                 dfs(i, 0, vis, a, delrow, delcol);
             }
             
-            if(a[i][m-1] == 'O' && vis[i][m-1]!=1)
+            if(a[i][m-1] == 'O' && vis[i][m-1]==0)
             {
                 dfs(i, m-1, vis, a, delrow, delcol);
             }
@@ -62,12 +84,12 @@ class Solution{
         
         for(int j=0;j<m;j++)
         {
-            if(a[0][j] == 'O' && vis[0][j]!=1)
+            if(a[0][j] == 'O' && vis[0][j]==0)
             {
                 dfs(0, j, vis, a, delrow, delcol);
             }
             
-            if(a[n-1][j] == 'O' && vis[n-1][j]!=1)
+            if(a[n-1][j] == 'O' && vis[n-1][j]==0)
             {
                 dfs(n-1, j, vis, a, delrow, delcol);
             }
@@ -78,27 +100,11 @@ class Solution{
             for(int j=0;j<m;j++)
             {
                 if(vis[i][j]==0 && a[i][j]=='O')
+                {
                     a[i][j] = 'X';
+                }
             }
         }
         return a;
-    }
-    
-    static void dfs(int row, int col, int[][] vis, char[][] a, int[] delrow, int[] delcol)
-    {
-        vis[row][col] = 1;
-        int r = a.length;
-        int c = a[0].length;
-        
-        for(int i=0;i<4;i++)
-        {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            
-            if(nrow>=0 && nrow<r && ncol>=0 && ncol<c && vis[nrow][ncol]==0 && a[nrow][ncol]=='O')
-            {
-                dfs(nrow, ncol, vis, a, delrow, delcol);
-            }
-        }
     }
 }
