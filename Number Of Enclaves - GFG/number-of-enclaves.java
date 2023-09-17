bf75c10694b010bf4d09d5c4a940f7ab52e3b1ca
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2023-09-16 22:59:35 -0400

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.java
+++ Number Of Enclaves - GFG/number-of-enclaves.java
@@ -34,6 +34,84 @@ class GFG {
 // } Driver Code Ends
 
 
+//DFS SOLUTION
+// class Solution {
+
+//     int numberOfEnclaves(int[][] grid) {
+
+//         // Your code here
+//         int r = grid.length;
+//         int c = grid[0].length;
+//         int[][] vis = new int[r][c];
+        
+        
+//         for(int i=0;i<r;i++)
+//         {
+//             if(vis[i][0]!=1 && grid[i][0]==1)
+//             {
+//                 vis[i][0] = 1;
+//                 dfs(i,0,r,c,vis,grid);
+//             }
+            
+//             if(vis[i][c-1]!=1 && grid[i][c-1]==1)
+//             {
+//                 vis[i][c-1] = 1;
+//                 dfs(i,c-1,r,c,vis,grid);
+//             }
+//         }
+        
+//         for(int j=0;j<c;j++)
+//         {
+//             if(vis[0][j]!=1 && grid[0][j]==1)
+//             {
+//                 vis[0][j] = 1;
+//                 dfs(0,j,r,c,vis,grid);
+//             }
+            
+//             if(vis[r-1][j]!=1 && grid[r-1][j]==1)
+//             {
+//                 vis[r-1][j] = 1;
+//                 dfs(r-1,j,r,c,vis,grid);
+//             }
+//         }
+        
+//         int count = 0;
+//         for(int i=0;i<r;i++)
+//         {
+//             for(int j=0;j<c;j++)
+//             {
+//                 if(grid[i][j]==1 && vis[i][j]!=1)
+//                 {
+//                   count++; 
+//                 }
+//             }
+//         }
+        
+//         return count;
+//     }
+    
+//     void dfs(int x, int y, int r, int c, int[][] vis, int[][] grid)
+//     {
+//         int[] delrow = {1, 0, -1, 0};
+//         int[] delcol = {0, 1, 0, -1};
+        
+//         for(int i=0;i<4;i++)
+//         {
+//             int nx = x + delrow[i];
+//             int ny = y + delcol[i];
+            
+//             if(nx>=0 && nx<r && ny>=0 && ny<c && grid[nx][ny]==1 && vis[nx][ny]!=1)
+//             {
+//                 vis[nx][ny]=1;
+//                 dfs(nx,ny,r,c,vis,grid);
+//             }
+//         }
+//     }
+    
+// }
+
+
+
 // User function Template for Java
 class Pair{
     int xcord;
@@ -47,7 +125,7 @@ class Pair{
 }
 
 
-//DFS SOLUTION
+//BFS SOLUTION
 class Solution {
 
     int numberOfEnclaves(int[][] grid) {
@@ -132,3 +210,4 @@ class Solution {
     }
     
 }
+
