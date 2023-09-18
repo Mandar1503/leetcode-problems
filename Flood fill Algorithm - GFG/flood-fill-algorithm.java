12be99f37af10e7eff0333bbd32ac5c075b6fea5
Mandar Kolhatkar <79890138+Mandar1503@users.noreply.github.com>
2023-09-18 04:50:14 -0400

    Added solution - LeetHub

---
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S1 = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int m = Integer.parseInt(S1[1]);
+            int[][] image =  new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S2 = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++)
+                    image[i][j] = Integer.parseInt(S2[j]);
+            }
+            String[] S3 = br.readLine().trim().split(" ");
+            int sr = Integer.parseInt(S3[0]);
+            int sc = Integer.parseInt(S3[1]);
+            int newColor = Integer.parseInt(S3[2]);
+            Solution obj = new Solution();
+            int[][] ans = obj.floodFill(image, sr, sc, newColor);
+            for(int i = 0; i < ans.length; i++){
+                for(int j = 0; j < ans[i].length; j++)
+                    System.out.print(ans[i][j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    private void dfs(int r, int c, int[][] vis, int[][] image, int oldColor, int newColor)
+    {
+        int n = image.length;
+        int m = image[0].length;
+        int[] delrow = {1,0,-1,0};
+        int[] delcol = {0,1,0,-1};
+        
+        for(int i=0;i<4;i++)
+        {
+            int nr = r + delrow[i];
+            int nc = c + delcol[i];
+                
+            if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]!=1 && image[nr][nc]==oldColor)
+            {
+                image[nr][nc] = newColor;
+                vis[nr][nc] = 1;
+                dfs(nr,nc,vis,image,oldColor,newColor);
+            }
+        }
+    }
+    
+    public int[][] floodFill(int[][] image, int sr, int sc,int newColor)
+    {
+        // Code here 
+        int n = image.length;
+        int m = image[0].length;
+        int[][] vis = new int[n][m];
+        int oldColor = image[sr][sc];
+        image[sr][sc] = newColor;
+        vis[sr][sc] = 1;
+        dfs(sr,sc,vis,image,oldColor,newColor);
+        return image;
+    }
+}
